1f24e4d4ed1e0232a6ba4668a9e012562cec52da
Nurdan Kayapinar <kesmennurdan@gmail.com>
2021-12-08 20:11:02 -0500

    new commit

--- src/my_git_repomy_git_repo/TenthJavaClass.java
+++ src/my_git_repomy_git_repo/TenthJavaClass.java
@@ -1,10 +1,33 @@
 package my_git_repomy_git_repo;
 
+
+
+import java.util.Arrays;
+
 public class TenthJavaClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// please find the biggest element in the given array
+		 int [] array = {1,24,2,342,123,543,54,3,2};
+		
+		
+		
+		int max = array[0];
+		 
+		for( int eachElement : array) {
+			
+			
+			if (max<= eachElement) {
+				
+			
+				max = eachElement;
+				
+			}
+		}
+		 
+		System.out.println(max);   // 543
+		
+		
 	}
-
 }
